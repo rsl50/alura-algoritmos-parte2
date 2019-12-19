3ec0c40cae23374d3acceac28b7839b5d16a8c53
Robson Lima <rsl.robson@gmail.com>
2019-12-19 09:26:09 -0300

    Implementando minha versão do mergeSort

--- src/br/com/alura/algoritmos/TestaMerge.java
+++ src/br/com/alura/algoritmos/TestaMerge.java
@@ -1,5 +1,7 @@
 package br.com.alura.algoritmos;
 
+import java.sql.SQLOutput;
+
 public class TestaMerge {
 
     public static void main(String[] args) {
@@ -18,7 +20,20 @@ public class TestaMerge {
                 new Nota("Ana", 10.0)
         };
 
-        Nota[] rank = intercala(notasDoAlberto, notasDoMauricio);
+        Nota[] notasDaSala = {
+                new Nota("Andre", 4.0),
+                new Nota("Mariana", 5.0),
+                new Nota("Carlos", 8.5),
+                new Nota("Paulo", 9.0),
+                new Nota("Jonas", 3.0),
+                new Nota("Juliana", 6.7),
+                new Nota("Guilherme", 7.0),
+                new Nota("Lucia", 9.3),
+                new Nota("Ana", 10.0)
+        };
+
+        //Nota[] rank = intercala(notasDoAlberto, notasDoMauricio);
+        Nota[] rank = mergeSort(notasDaSala);
         for (Nota nota: rank) {
             System.out.println(nota.getAluno() + " - " + nota.getNota());
         }
@@ -54,6 +69,47 @@ public class TestaMerge {
             resultado[atual] = notas2[atualNotas2];
             atualNotas2++;
             atual++;
+        }
+
+        return  resultado;
+    }
+
+    private static Nota[] mergeSort(Nota[] notas) {
+        int tamanho =  notas.length;
+        Nota[] resultado = new Nota[tamanho];
+
+        int atualNotas1 = 0;
+        int atualNotas2 = tamanho / 2;
+        int atual = 0;
+
+        while ((atualNotas1 <  (tamanho / 2)) && (atualNotas2 < tamanho)) {
+            if (notas[atualNotas1].getNota() < notas[atualNotas2].getNota()) {
+                resultado[atual] = notas[atualNotas1];
+                atualNotas1++;
+            } else {
+                resultado[atual] = notas[atualNotas2];
+                atualNotas2++;
+            }
+
+            if (atual < tamanho - 1) {
+                atual++;
+            }
+        }
+
+        while (atualNotas1 <  (tamanho / 2)) {
+            resultado[atual] = notas[atualNotas1];
+            atualNotas1++;
+            if (atual < tamanho - 1) {
+                atual++;
+            }
+        }
+
+        while (atualNotas2 < tamanho) {
+            resultado[atual] = notas[atualNotas2];
+            atualNotas2++;
+            if (atual < tamanho - 1) {
+                atual++;
+            }
         }
 
         return  resultado;
